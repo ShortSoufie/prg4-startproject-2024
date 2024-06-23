1460c00dfac86431f566a033743f1ddb34f7ec9d
ShortSoufie <shortsoufie@gmail.com>
2024-06-23 20:15:53 +0200

    lol

--- src/js/resources.js
+++ src/js/resources.js
@@ -2,15 +2,15 @@ import { ImageSource, Loader } from "excalibur";
 import { TiledResource } from "@excaliburjs/plugin-tiled";
 
 const Resources = {
-  Player: new ImageSource("/images/PlayerSpritesheet.png"),
-  npcBoy: new ImageSource("/images/npcBoy.png"),
-  npcGirl: new ImageSource("/images/npcGirl.png"),
-  enemySlime: new ImageSource("/images/Slime.png"),
-  enemyKingSlime: new ImageSource("/images/KingSlime.png"),
-  tutorialScreen: new ImageSource("/images/MenuExplain.png"),
-  menuScreen: new ImageSource("/images/MenuScreen.png"),
-  endScreen: new ImageSource("/images/EndScreen.png"),
-  World1Forest: new TiledResource("/images/TilemapTiled.tmx"),
+  Player: new ImageSource("images/PlayerSpritesheet.png"),
+  npcBoy: new ImageSource("images/npcBoy.png"),
+  npcGirl: new ImageSource("images/npcGirl.png"),
+  enemySlime: new ImageSource("images/Slime.png"),
+  enemyKingSlime: new ImageSource("images/KingSlime.png"),
+  tutorialScreen: new ImageSource("images/MenuExplain.png"),
+  menuScreen: new ImageSource("images/MenuScreen.png"),
+  endScreen: new ImageSource("images/EndScreen.png"),
+  World1Forest: new TiledResource("images/TilemapTiled.tmx"),
 };
 
 const ResourceLoader = new Loader([
